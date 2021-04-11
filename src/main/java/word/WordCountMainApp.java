11e9c41a1b51b98cfbb9137f3ba8f9f51ec7eac5
Murat <muratern@gmail.com>
2021-04-11 23:11:44 +0300

    iteration 8

--- src/main/java/word/WordCountMainApp.java
+++ src/main/java/word/WordCountMainApp.java
@@ -11,32 +11,30 @@ import word.ui.ReaderFactory;
 
 public class WordCountMainApp {
 
-	  
-	
 	public static void main(String[] args) {
+		new WordCountMainApp().run(args);
+	}
+
+	public void run(String[] args) {
 		IWriter print = new ConsoleWriter();
-		  
+
 		print.writeText("Enter the text:");
-		IReader input = ReaderFactory.getReader(args); 
-		 
+		IReader input = ReaderFactory.getReader(args);
+
 		FileReader fileReaderstops = new FileReader("stopwords.txt");
-		
+
 		StopWords stopWords = new StopWords(fileReaderstops);
 		WordInformation info = new WordInformation(stopWords);
- 		print.writeText(String.format("Number of words: %d, unique: %d; average word length:%.2f characters \n"
-				,Integer.toString(info.getValidWords(input.getText()).size()),
+		print.writeText(String.format("Number of words: %d, unique: %d; average word length:%.2f characters \n",
+				Integer.toString(info.getValidWords(input.getText()).size()),
 				Integer.toString(info.getUniqueValidWordsWitoutStopWords(input.getText()).size()),
 				AverageOperations.getAverageWordLength(info.getValidWordsWitoutStopWords(input.getText()))));
 
- 		 
- 		 if(ReaderFactory.indexParam) {
- 			print.writeText("Index:\n");
- 			info.getUniqueValidWordsWitoutStopWords(input.getText())
- 			.stream()
- 			.peek(x -> print.writeText(x+ "\n"));
- 		 
-         }
- 		 
+		if (ReaderFactory.indexParam) {
+			print.writeText("Index:\n");
+			info.getUniqueValidWordsWitoutStopWords(input.getText()).stream().peek(x -> print.writeText(x + "\n"));
+
+		}
 	}
 
 }

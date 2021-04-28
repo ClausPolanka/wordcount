23250029ce2b8ba3e5acfe9b7a2290edf7664de0
krisztian.sipos <krisztian.sipos@accenture.com>
2021-04-28 14:01:45 +0200

    iteration 2 (ali): - File input service added - stopwords.txt added

---
+++ src/main/java/hiring/wordcount/service/impl/FileInputService.java
@@ -0,0 +1,34 @@
+package hiring.wordcount.service.impl;
+
+import hiring.wordcount.service.InputReaderService;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.security.InvalidParameterException;
+import java.util.Scanner;
+
+public class FileInputService implements InputReaderService {
+    @Override
+    public String getServiceName() {
+        return "FileInputService";
+    }
+
+    @Override
+    public String read(String path) throws Exception {
+        if (path == null || path.isEmpty())
+            throw new InvalidParameterException("Path cannot be null or empty");
+
+        File file = new File(path);
+        if (!file.exists())
+            throw new FileNotFoundException(path);
+
+        StringBuilder result = new StringBuilder();
+
+        Scanner scanner = new Scanner(file);
+        while (scanner.hasNext()){
+            result.append(scanner.next()).append(" ");
+        }
+
+        return null;
+    }
+}

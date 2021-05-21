5ffc9b2a03d878187203df6793012a516f1b7a25
pavelmassa <massa.pavel@gmail.com>
2021-05-21 14:00:16 +0200

    Created InputInterface from file

---
+++ src/main/java/com/wordcount/ui/UserFileInputInterface.java
@@ -0,0 +1,26 @@
+package com.wordcount.ui;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class UserFileInputInterface implements InputInterface {
+
+    private final String pathToFile;
+
+    public UserFileInputInterface(String pathToFile) {
+        this.pathToFile = pathToFile;
+    }
+
+    @Override
+    public String getInput() {
+        try {
+            Path path = Paths.get(pathToFile);
+            return new String(Files.readAllBytes(path));
+        } catch (IOException e) {
+            System.err.printf("User Input file cannot be loaded. Cause: '%s'%n", e.getMessage());
+            throw new IllegalArgumentException("User Input file cannot be loaded", e);
+        }
+    }
+}

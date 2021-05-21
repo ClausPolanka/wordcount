c7e5b230cf614e9234c89afc5365cb581e07280e
pavelmassa <massa.pavel@gmail.com>
2021-05-21 11:47:33 +0200

    Using ScannerInputInterface

--- src/main/java/com/wordcount/WorkCounterApplication.java
+++ src/main/java/com/wordcount/WorkCounterApplication.java
@@ -1,12 +1,9 @@
 package com.wordcount;
 
-import java.util.Scanner;
-
 public class WorkCounterApplication {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        WordCounterUI wordCounterUI = new WordCounterUI(new WordCounter(), new SystemOutOutputInterface(), scanner::nextLine);
+        WordCounterUI wordCounterUI = new WordCounterUI(new WordCounter(), new SystemOutOutputInterface(), new ScannerInputInterface());
         wordCounterUI.countWords();
     }
 }

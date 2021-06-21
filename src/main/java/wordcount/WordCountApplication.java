f2163977dd44b3325eebf80e2852ac905751f195
Davide Pugliese <deviad@gmail.com>
2021-06-21 16:08:53 +0300

    Completed read lines from text

--- src/main/java/wordcount/WordCountApplication.java
+++ src/main/java/wordcount/WordCountApplication.java
@@ -9,16 +9,18 @@ import java.util.Optional;
 public class WordCountApplication {
 
 
-        public static void main(String ...args) {
+    public static void main(String... args) {
 
-                ITextService textService = new TextServiceFacade();
+        ITextService textService = new TextServiceFacade();
 
-                if(args.length > 0) {
-                        WordCountPrinter.printCount(textService.count(Optional.of(args[0])));
-                }
-                WordCountPrinter.printCount(textService.count(Optional.ofNullable(null)));
-
+        if (args.length > 0) {
+            WordCountPrinter.printCount(textService.count(Optional.of(args[0])));
+        } else {
+            WordCountPrinter.printCount(textService.count(Optional.ofNullable(null)));
         }
+
+
+    }
 
 
 }

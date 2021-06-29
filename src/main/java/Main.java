da438101236afde78ce2ee4efafbee0d532d3bd8
Luka Veljković <lukaveljkovic_95@hotmail.com>
2021-06-29 14:17:07 +0200

    Main class created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
-public class Foo {
+public class Main {
+
+    public static void main(String[] args) {
+
+        WordCount wordCount = new WordCount();
+        String text = "Dummy text";
+
+        System.out.println(wordCount.getNumberOfWords(text));
+    }
 }

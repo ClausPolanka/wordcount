0b148eb41e1349f9abce834188f9b9448aa3b950
Eni Sinanaj <enisinanaj@outlook.com>
2021-07-15 10:50:52 +0200

    First iteration of the word counter with user input from the terminal
    Signed-off-by: Eni Sinanaj <enisinanaj@outlook.com>

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,10 +1,31 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Foo {
-
     public int countWords(String input) {
         String[] strings = input.split("\\s");
-        return strings.length;
+
+        // check special chars within the word
+        int nonWords = notWordsCount(strings);
+
+        return strings.length - nonWords;
+    }
+
+    private int notWordsCount(String[] strings) {
+        int nonWords = 0;
+
+        for (String word: strings) {
+            if (isNonWord(word)) {
+                nonWords ++;
+            }
+        }
+
+        return nonWords;
+    }
+
+    private boolean isNonWord(String word) {
+        String[] nonWordParts = word.split("\\W");
+        if (nonWordParts.length > 1) {
+            return true;
+        }
+        return false;
     }
 }
